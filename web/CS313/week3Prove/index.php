696f78bb5a45edb550789b885f5f3f159ce6ebe7
Ann Halgren <annabellelarsen@gmail.com>
2018-10-04 07:54:33 -0700

    added colde for product display

--- web/CS313/week3Prove/index.php
+++ web/CS313/week3Prove/index.php
@@ -16,28 +16,28 @@
     <br/><br/><br/>
     <table>
         <tr>
-            <td id="col1">
+            <td class="col1">
                 <img src="fabric.jpg"/>
                 <br/>
                 <button onclick="addToCart('fabric')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('fabric')">Item Details</button>
             </td>
-            <td id="col2">
+            <td class="col2">
                 <img src="mat.jpg"/>
                 <br/>
                 <button onclick="addToCart('mat')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('mat')">Item Details</button>
             </td>
-            <td id="col3">
+            <td class="col3">
                 <img src="cutter.png"/>
                 <br/>
                 <button onclick="addToCart('rotary')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('rotary')">Item Details</button>
             </td>
-            <td id="col4">
+            <td class="col4">
                 <img src="ruler.jpg"/>
                 <br/>
                 <button onclick="addToCart('ruler')">Add to Cart</button>
@@ -46,28 +46,28 @@
             </td>
         </tr>
         <tr>
-            <td>
+            <td class="col1">
                 <img src="batting.png"/>
                 <br/>
                 <button onclick="addToCart('batting')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('batting')">Item Details</button>
             </td>
-            <td>
+            <td class="col2">
                 <img src="needles.jpg"/>
                 <br/>
                 <button onclick="addToCart('needles')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('needles')">Item Details</button>
             </td>
-            <td>
+            <td class="col3">
                 <img src="pins.png"/>
                 <br/>
                 <button onclick="addToCart('pins')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('pins')">Item Details</button>
             </td>
-            <td>
+            <td class="col4">
                 <img src="machine.jpg"/>
                 <br/>
                 <button onclick="addToCart('machine')">Add to Cart</button>
