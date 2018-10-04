58e347f46a1c75bcaa0ab8f87498fc50e668b826
Ann Halgren <annabellelarsen@gmail.com>
2018-10-04 08:13:54 -0700

    added code for product display

--- web/CS313/week3Prove/index.php
+++ web/CS313/week3Prove/index.php
@@ -18,7 +18,6 @@
         <tr>
             <td class="col1">
                 <img src="fabric.jpg"/>
-                <br/>
                 <button onclick="addToCart('fabric')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('fabric')">Item Details</button>
@@ -32,14 +31,12 @@
             </td>
             <td class="col3">
                 <img src="cutter.png"/>
-                <br/>
                 <button onclick="addToCart('rotary')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('rotary')">Item Details</button>
             </td>
             <td class="col4">
                 <img src="ruler.jpg"/>
-                <br/>
                 <button onclick="addToCart('ruler')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('ruler')">Item Details</button>
@@ -48,28 +45,24 @@
         <tr>
             <td class="col1">
                 <img src="batting.png"/>
-                <br/>
                 <button onclick="addToCart('batting')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('batting')">Item Details</button>
             </td>
             <td class="col2">
                 <img src="needles.jpg"/>
-                <br/>
                 <button onclick="addToCart('needles')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('needles')">Item Details</button>
             </td>
             <td class="col3">
                 <img src="pins.jpg"/>
-                <br/>
                 <button onclick="addToCart('pins')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('pins')">Item Details</button>
             </td>
             <td class="col4">
                 <img src="machine.jpg"/>
-                <br/>
                 <button onclick="addToCart('machine')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('machine')">Item Details</button>
