1533fd2ffd20578dc758c0e1496530ea699300d4
Ann Halgren <annabellelarsen@gmail.com>
2018-10-04 08:03:48 -0700

    added code for product display

--- web/CS313/week3Prove/index.php
+++ web/CS313/week3Prove/index.php
@@ -45,7 +45,36 @@
                 <button onclick="getDetails('ruler')">Item Details</button>
             </td>
         </tr>
-        
+        <tr>
+            <td class="col1">
+                <img src="batting.png"/>
+                <br/>
+                <button onclick="addToCart('batting')">Add to Cart</button>
+                <br/>
+                <button onclick="getDetails('batting')">Item Details</button>
+            </td>
+            <td class="col2">
+                <img src="needles.jpg"/>
+                <br/>
+                <button onclick="addToCart('needles')">Add to Cart</button>
+                <br/>
+                <button onclick="getDetails('needles')">Item Details</button>
+            </td>
+            <td class="col3">
+                <img src="pins.png"/>
+                <br/>
+                <button onclick="addToCart('pins')">Add to Cart</button>
+                <br/>
+                <button onclick="getDetails('pins')">Item Details</button>
+            </td>
+            <td class="col4">
+                <img src="machine.jpg"/>
+                <br/>
+                <button onclick="addToCart('machine')">Add to Cart</button>
+                <br/>
+                <button onclick="getDetails('machine')">Item Details</button>
+            </td>
+        </tr>
     </table>
     
 
