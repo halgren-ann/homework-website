02f0aa5a673f201336e0c31178a24ede881bc0f1
Ann Halgren <annabellelarsen@gmail.com>
2018-10-17 12:29:34 -0700

    Adding w3schools to-do list functionality to TaskMe.php

--- web/CS313/TaskMe/TaskMe.php
+++ web/CS313/TaskMe/TaskMe.php
@@ -34,11 +34,7 @@
   </div>
 </div>
 
-<div class="split right">
-  <div class="bordered">
-    <h2>Recently added tasks:</h2>
-    <hr>
-    <ul>
+<ul>
         <li>Hit the gym</li>
         <li>Pay bills</li>
         <li>Meet George</li>
@@ -47,70 +43,10 @@
         <li>Organize office</li>
     </ul>
     
-  </div>
-</div>
 
 <div class="titleArea">
   <h1><span style="font-family: 'Caveat', cursive;">Hello, Ann - </span>TaskMe</h1>
 </div>
-
-<script>
-    // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
-</script>
      
 </body>
 </html> 
