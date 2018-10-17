df6b02091bc51d3257363aa6e4667139f5f9024b
Ann Halgren <annabellelarsen@gmail.com>
2018-10-17 00:40:43 -0700

    Adding files for waldo demonstration

--- web/CIT261/waldo/script.js
+++ web/CIT261/waldo/script.js
@@ -1,9 +1,13 @@
 function myFunction(item) {
     //capture the chosen item
     var child = document.getElementById(item);
+    var parent = child.parentNode;
     //remove the chosen item
-    child.parentNode.removeChild(child);
+    parent.removeChild(child);
     //add the item back in at a new location
     var location = (Math.floor(Math.random() * 6)) * 2;
-    child.parentNode.insertBefore(child, list.childNodes[location]);
+    var btn = document.createElement("BUTTON");        // Create a <button> element
+    var t = document.createTextNode(item);       // Create a text node
+    btn.appendChild(t);                                // Append the text to <button>
+    parent.insertBefore(child, parent.childNodes[location]);
 }
