51359c142d088e3f1d1d7b4927fbbd1b259ca679
Ann Halgren <annabellelarsen@gmail.com>
2018-10-17 11:36:17 -0700

    Adding w3schools to-do list functionality to TaskMe.php

--- web/CS313/TaskMe/TaskMe.php
+++ web/CS313/TaskMe/TaskMe.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://fonts.googleapis.com/css?family=Yesteryear" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
-<link href="styles.css" rel="stylesheet">
-<script src="script.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Yesteryear" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+    <script src="script.js"></script>
 </head>
 <body>
 
