517eeefa4b182b0c8df0cf34899f6df93230103c
Ann Halgren <annabellelarsen@gmail.com>
2018-10-17 13:22:45 -0700

    Adding functionality to the login page

--- web/CS313/TaskMe/login.php
+++ web/CS313/TaskMe/login.php
@@ -8,14 +8,16 @@
     <link href="whole-styles.css" rel="stylesheet">
 </head>
 <body>
-    <label class="centered">Username:</label><br>
-    <input type="text" name="username" placeholder="Enter username" maxlength=16 required><br>
-    <label class="centered">Password:</label><br>
-    <input type="text" name="user_password" placeholder="Enter password" maxlength=16 required><br>
-    <input type="submit">
-
+    <div class="centered">
+        <br><br><br><br><br><br>
+        <label>Username:</label><br>
+        <input type="text" name="username" placeholder="Enter username" maxlength=16 required><br>
+        <label>Password:</label><br>
+        <input type="text" name="user_password" placeholder="Enter password" maxlength=16 required><br>
+        <input type="submit" style="width:100%">
+    </div>
     <div class="titleArea">
-    <h1><span style="font-family: 'Caveat', cursive;">Hello, Ann - </span>TaskMe</h1>
+        <h1>TaskMe</h1>
     </div>
      
 </body>
