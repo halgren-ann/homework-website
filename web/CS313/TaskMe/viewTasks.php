cac6e06c8fa0f1ee4f53b25e037dd11272385c76
Ann Halgren <annabellelarsen@gmail.com>
2018-10-17 16:56:59 -0700

    Adding outline of the viewTasks.php page

---
+++ web/CS313/TaskMe/viewTasks.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TaskMe</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" type="text/css" href="split-styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Yesteryear" rel="stylesheet">
+  </head>
+  <body>
+        <div class="split left">
+            <div class="centered">
+                <br><br><button><a href="TaskMe.php">Home</a></button><br><br><br>
+                <ul>
+                    <li>Hit the gym - Due Date</li>
+                    <li>Pay bills</li>
+                    <li>Meet George - Due Date</li>
+                </ul>
+            </div>
+        </div>
+            
+        <div class="split right">
+            <div class="centered">
+                <br><br><button><a href="addTask.php">Add Task</a></button><br><br><br>
+                <ul>
+                    <li>Buy eggs</li>
+                    <li>Read a book</li>
+                    <li>Organize office</li>
+                </ul>
+            </div>
+        </div>
+            
+        <div class="titleArea">
+        <h1>TaskMe</h1>
+        </div>
+
+  </body>
+</html>
