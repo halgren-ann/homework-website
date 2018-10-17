96be28fe4c988b61fa6b1662e0bc06fc729c0448
Ann Halgren <annabellelarsen@gmail.com>
2018-10-17 14:27:41 -0700

    Adding functionality to the addTask.php page

---
+++ web/CS313/TaskMe/addTask.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TaskMe</title>
+    <link href="https://fonts.googleapis.com/css?family=Yesteryear" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+    <link href="whole-styles.css" rel="stylesheet">
+</head>
+<body>
+    <div class="centered">
+        <br><br><br><br><br><br>
+        <input type="text" name="task" placeholder="Enter the main task here" required><br><br><br>
+        <p>     </p><input type="text" name="subtask1" placeholder="Enter optional subtask or step"><br><br><br>
+        <p>     </p><input type="text" name="subtask2" placeholder="Enter optional subtask or step"><br><br><br>
+        <p>     </p><input type="text" name="subtask3" placeholder="Enter optional subtask or step"><br><br><br>
+        <p>     </p><input type="text" name="subtask4" placeholder="Enter optional subtask or step"><br><br><br>
+        <select class="dropdown">
+            <option value="default">Choose category</option>
+            <option value="urgent">This is urgent</option>
+            <option value="regular">Regular task</option>
+            <option value="goal">This is a goal</option>
+        </select>
+        <br><br><br>
+        <select class="dropdown">
+            <option value="default">Choose difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+        </select>
+        <br><br><br>
+        <label>Optional: Due Date</label><br>
+        <input type="text" name="date_due_month" placeholder="Enter month MM" maxlength=2>
+        <p>     </p>
+        <input type="text" name="date_due_day" placeholder="Enter day DD" maxlength=2>
+        <p>     </p>
+        <input type="text" name="date_due_year" placeholder="Enter year YYYY" maxlength=4><br><br><br>
+        <input type="submit" style="width:100%">
+    </div>
+    <div class="titleArea">
+        <h1>TaskMe</h1>
+    </div>
+     
+</body>
+</html> 
