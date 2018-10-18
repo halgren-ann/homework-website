8ff3e6da9589a0348e018c1cb6d297a6c27944bc
Ann Halgren <annabellelarsen@gmail.com>
2018-10-17 17:46:11 -0700

    Adding outline of the viewTasks.php page

--- web/CS313/TaskMe/viewTasks.php
+++ web/CS313/TaskMe/viewTasks.php
@@ -3,36 +3,42 @@
   <head>
     <meta charset="utf-8">
     <title>TaskMe</title>
-    <script src="script.js"></script>
-    <link rel="stylesheet" type="text/css" href="split-styles.css">
+    <link rel="stylesheet" type="text/css" href="whole-styles.css">
     <link href="https://fonts.googleapis.com/css?family=Yesteryear" rel="stylesheet">
   </head>
   <body>
-        <div class="split left">
-            <div class="centered">
-                <button><a href="TaskMe.php">Home</a></button><br><br><br>
+        <br><br><br><button><a href="TaskMe.php">Home</a></button>
+        <br><br><button><a href="addTask.php">Add Task</a></button>
+
+        <ul>
+            <li>Hit the gym - Due Date</li>
+            <li>Pay bills
                 <ul>
-                    <li>Hit the gym - Due Date</li>
-                    <li>Pay bills</li>
-                    <li>Meet George - Due Date</li>
-                </ul>
-            </div>
-        </div>
-            
-        <div class="split right">
-            <div class="centered">
-                <button><a href="addTask.php">Add Task</a></button><br><br><br>
-                <ul>
-                    <li>Buy eggs</li>
-                    <li>Read a book</li>
-                    <li>Organize office</li>
-                </ul>
-            </div>
-        </div>
+                    <li>Internet</li>
+                    <li>Waste Management</li>
+                    <li>Water</li>
+                    <li>Electricity</li>
+                </ul> 
+            </li>
+            <li>Meet George - Due Date</li>
+            <li>Buy eggs</li>
+            <li>Read a book</li>
+            <li>Organize office</li>
+        </ul>
             
         <div class="titleArea">
-        <h1>TaskMe</h1>
+            <h1>TaskMe</h1>
         </div>
+
+        <script>
+            // Add a "checked" symbol when clicking on a list item
+            var list = document.querySelector('ul');
+            list.addEventListener('click', function(ev) {
+                if (ev.target.tagName === 'LI') {
+                    ev.target.classList.toggle('checked');
+                }
+            }, false);
+        </script>
 
   </body>
 </html>
