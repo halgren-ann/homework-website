edf68675fa1be19509beba9f175c39acce3b90c7
Ann Halgren <annabellelarsen@gmail.com>
2018-10-20 23:02:14 -0700

    Trying to add recently added tasks to TaskMe.php

--- web/CS313/TaskMe/verifyLogin.php
+++ web/CS313/TaskMe/verifyLogin.php
@@ -9,25 +9,7 @@
 
 <?php
     //connect to the database
-     try {
-        $dbUrl = getenv('DATABASE_URL');
-
-        $dbOpts = parse_url($dbUrl);
-
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex) {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-    }
+    include 'dbConnect.php';
 
     // define variables
     $username = $_POST["username"];
