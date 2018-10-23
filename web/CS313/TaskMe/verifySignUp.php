032145bf0e35924d688de3d274cb16b4f29c923d
Ann Halgren <annabellelarsen@gmail.com>
2018-10-23 12:14:15 -0700

    Working on adding information posted from signUp.php to the database

--- web/CS313/TaskMe/verifySignUp.php
+++ web/CS313/TaskMe/verifySignUp.php
@@ -25,8 +25,13 @@
         return $data;
     }
 
+    //PREPARE fooplan (int, text, bool, numeric) AS
+    //INSERT INTO foo VALUES($1, $2, $3, $4);
+    //EXECUTE fooplan(1, 'Hunter Valley', 't', 200.00);
+
     //now insert the values into the database
-    $stmt = $db->prepare('INSERT into public.user(username, user_password, first_name, last_name, display_color) VALUES (username=:username, user_password=:user_password, first_name=:first_name, last_name=:last_name, display_color=:display_color);');
+    $stmt = $db->prepare('INSERT into public.user(username, user_password, first_name, last_name, display_color) 
+        VALUES (:username, :user_password, :first_name, :last_name, :display_color);');
     $stmt->execute(array(':username' => $username, ':user_password' => $user_password, ':first_name' => $first_name, ':last_name' => $last_name, ':display_color' => $display_color);
     //also capture the user's id for use in this session
     $_SESSION["user_id"] = $pdo->lastInsertId('public.user_id_seq');
