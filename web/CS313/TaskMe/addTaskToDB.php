a166e229837d194165641b8a9f546aa783787db4
Ann Halgren <annabellelarsen@gmail.com>
2018-10-23 22:33:51 -0700

    working on functionality of addTask.php and addTaskToDB.php pages

--- web/CS313/TaskMe/addTaskToDB.php
+++ web/CS313/TaskMe/addTaskToDB.php
@@ -31,12 +31,7 @@
     $stmt->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_STR);
     $stmt->bindValue(':task_text', $task, PDO::PARAM_STR);
     $stmt->bindValue(':date_added', date('Y-m-d'), PDO::PARAM_STR);
-    if ($date_due_year != "") {
-        $stmt->bindValue(':date_due', date_format(strtotime($totalDate), 'Y-m-d'), PDO::PARAM_STR);
-    }
-    else {
-        $stmt->bindValue(':date_added', 'NULL', PDO::PARAM_STR);
-    }
+    $stmt->bindValue(':date_due', date_format(strtotime($totalDate), 'Y-m-d'), PDO::PARAM_STR);
     $stmt->bindValue(':classification', $classification, PDO::PARAM_STR);
     $stmt->bindValue(':difficulty', $difficulty, PDO::PARAM_STR);
     $stmt->bindValue(':is_complete', 'false', PDO::PARAM_STR);
