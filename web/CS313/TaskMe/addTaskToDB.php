86e28a088d2196cd0d328d995def601c6154e700
Ann Halgren <annabellelarsen@gmail.com>
2018-10-23 17:49:49 -0700

    working on functionality of addTask.php and addTaskToDB.php pages

--- web/CS313/TaskMe/addTaskToDB.php
+++ web/CS313/TaskMe/addTaskToDB.php
@@ -39,10 +39,46 @@
 
     //capture this task_id
     $task_id = $db->lastInsertId('task_id_seq');
-    echo $task_id;
 
     //insert any subtasks
     if (isset($_POST["subtask1"])) {
-        
+        $stmt = $db->prepare('INSERT into public.subtask(user_id, task_id, task_text, is_complete) 
+            VALUES (:user_id, :task_id, :task_text, :is_complete);');
+        $stmt->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_STR);
+        $stmt->bindValue(':task_id', $task_id, PDO::PARAM_STR);
+        $stmt->bindValue(':task_text', $subtask1, PDO::PARAM_STR);
+        $stmt->bindValue(':is_complete', 'false', PDO::PARAM_STR);
+        $stmt->execute();
     }
+    if (isset($_POST["subtask2"])) {
+        $stmt = $db->prepare('INSERT into public.subtask(user_id, task_id, task_text, is_complete) 
+            VALUES (:user_id, :task_id, :task_text, :is_complete);');
+        $stmt->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_STR);
+        $stmt->bindValue(':task_id', $task_id, PDO::PARAM_STR);
+        $stmt->bindValue(':task_text', $subtask2, PDO::PARAM_STR);
+        $stmt->bindValue(':is_complete', 'false', PDO::PARAM_STR);
+        $stmt->execute();
+    }
+    if (isset($_POST["subtask3"])) {
+        $stmt = $db->prepare('INSERT into public.subtask(user_id, task_id, task_text, is_complete) 
+            VALUES (:user_id, :task_id, :task_text, :is_complete);');
+        $stmt->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_STR);
+        $stmt->bindValue(':task_id', $task_id, PDO::PARAM_STR);
+        $stmt->bindValue(':task_text', $subtask3, PDO::PARAM_STR);
+        $stmt->bindValue(':is_complete', 'false', PDO::PARAM_STR);
+        $stmt->execute();
+    }
+    if (isset($_POST["subtask4"])) {
+        $stmt = $db->prepare('INSERT into public.subtask(user_id, task_id, task_text, is_complete) 
+            VALUES (:user_id, :task_id, :task_text, :is_complete);');
+        $stmt->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_STR);
+        $stmt->bindValue(':task_id', $task_id, PDO::PARAM_STR);
+        $stmt->bindValue(':task_text', $subtask4, PDO::PARAM_STR);
+        $stmt->bindValue(':is_complete', 'false', PDO::PARAM_STR);
+        $stmt->execute();
+    }
+
+    //redirect the page to TaskMe.php
+    header("Location: TaskMe.php");
+    die();
 ?>
