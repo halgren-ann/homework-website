afa085a099c87e7b3e46a6e641d53c2505380bd7
Ann Halgren <annabellelarsen@gmail.com>
2018-10-26 16:49:33 -0700

    Working on changing is_complete in the database when I check/uncheck a to-do list item

--- web/CS313/TaskMe/isChecked.php
+++ web/CS313/TaskMe/isChecked.php
@@ -1,14 +1,17 @@
 <?php
     $task_text = file_get_contents('php://input');
     include 'dbConnect.php';
+    session_start();
 
-    $stmt = $db->prepare('UPDATE public.task SET is_complete = :is_complete WHERE task_text = :task_text');
+    $stmt = $db->prepare('UPDATE public.task SET is_complete = :is_complete WHERE task_text = :task_text AND user_id = :user_id');
     $stmt->bindValue(':is_complete', true);
     $stmt->bindValue(':task_text', $task_text, PDO::PARAM_STR);
+    $stmt->bindValue(':user_id', $_SESSION["user_id"]);
     $stmt->execute();
 
-    $stmt = $db->prepare('UPDATE public.subtask SET is_complete = :is_complete WHERE task_text = :task_text');
+    $stmt = $db->prepare('UPDATE public.subtask SET is_complete = :is_complete WHERE task_text = :task_text AND user_id = :user_id');
     $stmt->bindValue(':is_complete', true);
     $stmt->bindValue(':task_text', $task_text, PDO::PARAM_STR);
+    $stmt->bindValue(':user_id', $_SESSION["user_id"]);
     $stmt->execute();
 ?>
