95cb1dc75e55a70577570738394c4439e397ae99
Ann Halgren <annabellelarsen@gmail.com>
2018-10-26 14:49:57 -0700

    Working on changing is_complete in the database when I check/uncheck a to-do list item

--- web/CS313/TaskMe/cleanupDB.php
+++ web/CS313/TaskMe/cleanupDB.php
@@ -2,5 +2,5 @@
     include 'dbConnect.php';
 
     $stmt = $db->prepare('DELETE FROM public.subtask WHERE is_complete = :is_complete');
-    $stmt->execute(array(':is_complete' => 'true'));
+    $stmt->execute(array(':is_complete' => true));
 ?>
--- web/CS313/TaskMe/isChecked.php
+++ web/CS313/TaskMe/isChecked.php
@@ -3,5 +3,7 @@
     include 'dbConnect.php';
 
     $stmt = $db->prepare('UPDATE public.subtask SET is_complete = :is_complete WHERE task_text = :task_text');
-    $stmt->execute(array(':task_text' => $task_text, ':is_complete' => 'true'));
+    $stmt->bindValue(':is_complete', true);
+    $stmt->bindValue(':task_text', $task_text, PDO::PARAM_STR);
+    $stmt->execute();
 ?>
