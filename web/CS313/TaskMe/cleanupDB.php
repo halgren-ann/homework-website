0b4d3f46cd559632e4b0c755c5b65acc55c8fc9d
Ann Halgren <annabellelarsen@gmail.com>
2018-10-26 17:07:08 -0700

    Working on changing is_complete in the database when I check/uncheck a to-do list item

--- web/CS313/TaskMe/cleanupDB.php
+++ web/CS313/TaskMe/cleanupDB.php
@@ -1,27 +1,30 @@
 <?php
     include 'dbConnect.php';
     session_start();
+    $user_id = $_SESSION["user_id"];
 
     $stmt = $db->prepare('DELETE FROM public.subtask WHERE is_complete = :is_complete AND user_id = :user_id');
     $stmt->bindValue(':is_complete', true);
-    $stmt->bindValue(':user_id', $_SESSION["user_id"]);
+    $stmt->bindValue(':user_id', $user_id);
     $stmt->execute();
     
     //If it's a main task that was deleted, have to delete all the subtasks first
     $stmt = $db->prepare('SELECT * FROM public.task WHERE user_id = :user_id AND is_complete = :is_complete;');
-    $stmt->execute(array(':user_id' => $_SESSION["user_id"], ':is_complete' => true));
+    $stmt->bindValue(':is_complete', true);
+    $stmt->bindValue(':user_id', $user_id);
+    $stmt->execute();
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     foreach($rows as $row) {
         $task_id = $row["id"];
         $stmt = $db->prepare('DELETE public.subtask WHERE task_id = :task_id AND user_id = :user_id');
         $stmt->bindValue(':task_id', $task_id);
-        $stmt->bindValue(':user_id', $_SESSION["user_id"]);
+        $stmt->bindValue(':user_id', $user_id);
         $stmt->execute();
     }
     
     $stmt = $db->prepare('DELETE FROM public.task WHERE is_complete = :is_complete AND user_id = :user_id');
     $stmt->bindValue(':is_complete', true);
-    $stmt->bindValue(':user_id', $_SESSION["user_id"]);
+    $stmt->bindValue(':user_id', $user_id);
     $stmt->execute();
 ?>
