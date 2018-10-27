40ea31fe3b1f289442c85fea993b95cc39389a97
Ann Halgren <annabellelarsen@gmail.com>
2018-10-27 13:44:27 -0700

    Trying to get initial canvas background down.

--- web/CIT261/FoodMaze/script.js
+++ web/CIT261/FoodMaze/script.js
@@ -25,16 +25,12 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 drawCheckeredBackground(canvas, 4, 6);
 
 /*
+var cell_width = 100;
+var cell_height = 100;
+var x_offset = 100;
+var y_offset = 100;
 
-var cell_width = 40;
-var cell_height = 40;
-var x_offset = 40;
-var y_offset = 40;
-var nRow = 4;
-var nCol = 6;
-*/
 
-/*
 function Coordinate(x, y) {
     this.x = x;
     this.y = y;
@@ -44,6 +40,7 @@ function Coordinate(x, y) {
 function Food() {}
 Food.prototype.width = cell_width;
 Food.prototype.height = cell_height;
+Food.location = new Coordinate();
 Food.prototype.hideFood = function(element) {
     element.classList.add('hidden');
 }
@@ -83,55 +80,46 @@ Coffee.locations = [
 ];
 */
 
-/*
-function drawCheckeredBackground(can, nRow, nCol) {
-    var ctx = can.getContext("2d");
-    var w = can.width;
-    var h = can.height;
-
-    nRow = nRow || 8;    // default number of rows
-    nCol = nCol || 8;    // default number of columns
-
-    w /= nCol;            // width of a block
-    h /= nRow;            // height of a block
-
-    for (var i = 0; i < nRow; ++i) {
-        for (var j = 0, col = nCol / 2; j < col; ++j) {
-            ctx.rect(2 * j * w + (i % 2 ? 0 : w), i * h, w, h);
-        }
-    }
-
-    //ctx.fillStyle = "#D2691E";
-    ctx.fill();
-}
-
-
-    var canvas = document.getElementById("canvas");
-    //var ctx = canvas.getContext("2d");
-    //ctx.fillStyle = "#228B22";
-    //ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    drawCheckeredBackground(canvas, nRow, noCol);
-
-
-*/
+var newImage = document.createElement("IMG");
+    newImage.src = "broccoli.png";
+    STYLE="position:absolute; TOP:100px; LEFT:100px; WIDTH:100px; HEIGHT:100px"; 
+    var element = document.getElementById("gameboard");
+    element.appendChild(newImage);
 
 /*
-window.onload=function() {
-    var greenArray = document.getElementsByClassName("green");
-    for (var i=0; i<greenArray.length; i++) {
-        var one = greenArray[i];
-        var ctx = one.getContext("2d");
-        ctx.fillStyle = "#228B22";
-        ctx.fillRect(0, 0, 80, 100);
-    }
-
-    var brownArray = document.getElementsByClassName("brown");
-    for (var i=0; i<brownArray.length; i++) {
-        var one = brownArray[i];
-        var ctx = one.getContext("2d");
-        ctx.fillStyle = "#D2691E";
-        ctx.fillRect(100, 100, 80, 100);
+//make the array of food objects
+var foodArray;
+for (var i=0; i<3; i++) {
+		var newBroccoli = new Broccoli();
+    newBroccoli.location = newBroccoli.locations[i];
+    foodArray[(i*3)] = newBroccoli;
+    var newBread = new Bread();
+    newBread.location = newBread.locations[i];
+    foodArray[(i*3) + 1] = newBread;
+    var newCoffee = new Coffee();
+    newCoffee.location = newCoffee.locations[i];
+    foodArray[(i*3) + 2] = newCoffee;
 }
+
+//now draw the food objects on the board
+for (var i=0; i<foodArray.length; i++) {
+		var newImage = document.createElement("IMG");
+    if (typeOf(foodArray[i]) == "Broccoli") {
+    		newImage.src = "broccoli.png";
+    }
+    else if (typeOf(foodArray[i]) == "Bread") {
+    		newImage.src = "bread.png";
+    }
+    else {
+    		newImage.src = "coffee.png";
+    }
+    //newImage.width = foodArray[i].width;
+    //newImage.height = foodArray[i].height;
+    newImage.style = "position:absolute; top:" + foodArray[i].location.y + "px; left:" + foodArray[i].locations.x + "px";
+    var element = document.getElementById("gameboard");
+    element.appendChild(newImage);
 }
 */
+
+
+
