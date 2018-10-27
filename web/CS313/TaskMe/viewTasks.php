ecca616616bb6271f4aa5635daa340696164b598
Ann Halgren <annabellelarsen@gmail.com>
2018-10-26 21:40:45 -0700

    Working on changing is_complete in the database when I check/uncheck a to-do list item

--- web/CS313/TaskMe/viewTasks.php
+++ web/CS313/TaskMe/viewTasks.php
@@ -58,10 +58,7 @@
                         foreach ($rows as $row) {
                             //For each task
                             echo "<li>" . $row["task_text"];
-                            //check if there is a due date
-                            if ($row["date_due"] != NULL) {
-                                echo " - Due " . $row["date_due"];
-                            }       
+
                             //check for subtasks associated with this task
                             $stmt = $db->prepare('SELECT * FROM public.subtask WHERE task_id = :task_id');
                             $stmt->execute(array(':task_id' => $row["id"]));
@@ -100,10 +97,7 @@
                         foreach ($rows as $row) {
                             //For each task
                             echo "<li>" . $row["task_text"];
-                            //check if there is a due date
-                            if ($row["date_due"] != NULL) {
-                                echo " - Due " . $row["date_due"];
-                            }       
+                                  
                             //check for subtasks associated with this task
                             $stmt = $db->prepare('SELECT * FROM public.subtask WHERE task_id = :task_id');
                             $stmt->execute(array(':task_id' => $row["id"]));
@@ -133,10 +127,7 @@
                         foreach ($rows as $row) {
                             //For each task
                             echo "<li>" . $row["task_text"];
-                            //check if there is a due date
-                            if ($row["date_due"] != NULL) {
-                                echo " - Due " . $row["date_due"];
-                            }       
+                                  
                             //check for subtasks associated with this task
                             $stmt = $db->prepare('SELECT * FROM public.subtask WHERE task_id = :task_id');
                             $stmt->execute(array(':task_id' => $row["id"]));
@@ -166,10 +157,7 @@
                         foreach ($rows as $row) {
                             //For each task
                             echo "<li>" . $row["task_text"];
-                            //check if there is a due date
-                            if ($row["date_due"] != NULL) {
-                                echo " - Due " . $row["date_due"];
-                            }       
+                                   
                             //check for subtasks associated with this task
                             $stmt = $db->prepare('SELECT * FROM public.subtask WHERE task_id = :task_id');
                             $stmt->execute(array(':task_id' => $row["id"]));
@@ -199,10 +187,7 @@
                         foreach ($rows as $row) {
                             //For each task
                             echo "<li>" . $row["task_text"];
-                            //check if there is a due date
-                            if ($row["date_due"] != NULL) {
-                                echo " - Due " . $row["date_due"];
-                            }       
+                                   
                             //check for subtasks associated with this task
                             $stmt = $db->prepare('SELECT * FROM public.subtask WHERE task_id = :task_id');
                             $stmt->execute(array(':task_id' => $row["id"]));
