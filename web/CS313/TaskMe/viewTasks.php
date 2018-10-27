dfc9b136ab15b2d2c29636aa0f062c83930bd3dd
Ann Halgren <annabellelarsen@gmail.com>
2018-10-26 17:54:46 -0700

    Working on changing is_complete in the database when I check/uncheck a to-do list item

--- web/CS313/TaskMe/viewTasks.php
+++ web/CS313/TaskMe/viewTasks.php
@@ -242,7 +242,7 @@
 
         <script>
             // Add a "checked" symbol when clicking on a list item
-            var list = document.querySelector('ul');
+            var list = document.querySelector('li');
             list.addEventListener('click', function(ev) {
                 if (ev.target.tagName === 'LI') {
                     ev.target.classList.add('checked');
