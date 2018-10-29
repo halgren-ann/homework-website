e1f3b3035e446c7b603630df03e4c27da02df7a4
Ann Halgren <annabellelarsen@gmail.com>
2018-10-29 14:35:32 -0700

    Working on the action of eating food

--- web/CIT261/FoodMaze/script.js
+++ web/CIT261/FoodMaze/script.js
@@ -24,11 +24,38 @@ ctx.fillStyle = "#FFFF00";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 drawCheckeredBackground(canvas, 4, 6);
 
-//Create the food object arrays
-function Coordinate(x, y) {
-    this.x = x;
-    this.y = y;
+//Create the food objects
+function Coordinate(top, left) {
+    this.top = top;
+    this.left = left;
 }
+
+function Brocolli(top, left, name) {
+    this.location = new Coordinate(top, left);
+    this.isGood = true;
+    this.audioFile = "blip.wav";
+    this.name = name;
+}
+
+function Bread(top,left, name) {
+    this.location = new Coordinate(top, left);
+    this.isGood = true;
+    this.audioFile = "blip.wav";
+    this.name = name;
+}
+
+function Coffee(top, left, name) {
+    this.location = new Coordinate(top, left);
+    this.isGood = false;
+    this.audioFile = "buzzer.wav";
+    this.name = name;
+}
+
+//Create the array of food objects
+var foodArray = new Array();
+foodArray.push(new Broccoli(8, 508, "broccoli1"));
+foodArray.push(new Broccoli(108, 308, "broccoli2"));
+//finish.....
 
 
 //Moving the gamepiece and label
@@ -44,9 +71,7 @@ function checkKey(e) {
         // up arrow
         if (currentTop > 50) {
             document.getElementById("gamepiece").style.top = (currentTop - 100) + "px";
-            document.getElementById("youAreHere").style.top = (currentTop - 82) + "px";
-            gamepiece.classList.add("changeRed");
-            setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeRed", ""); }, 1000);            
+            document.getElementById("youAreHere").style.top = (currentTop - 82) + "px";            
         }
     }
     else if (e.keyCode == '40') {
@@ -54,8 +79,6 @@ function checkKey(e) {
         if (currentTop < 250) {
             document.getElementById("gamepiece").style.top = (currentTop + 100) + "px";
             document.getElementById("youAreHere").style.top = (currentTop + 118) + "px";
-            gamepiece.classList.add("changeGreen");
-            setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeGreen", ""); }, 1000);
         }
     }
     else if (e.keyCode == '37') {
@@ -72,6 +95,35 @@ function checkKey(e) {
             document.getElementById("youAreHere").style.left = (currentLeft + 200) + "px";
         }
     }
+
+    checkLocation();
+}
+
+function checkLocation() {
+    var gamepiece = document.getElementById("gamepiece");
+    for (var i=0; i<foodArray.length; i++) {
+        if (parseInt(gamepiece.style.top, 10) == foodArray[i].location.top) {
+            if (parseInt(gamepiece.style.left, 10) == foodArray[i].location.left) {
+                //You just ate something
+                //Turn colors and scale
+                if (foodArray[i].isGood) {
+                    gamepiece.classList.add("changeGreen");
+                    
+                    setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeGreen", ""); }, 1000);
+                }
+                else {
+                    gamepiece.classList.add("changeRed");
+                    setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeRed", ""); }, 1000);
+                }
+                //hide this image
+                document.getElementById(foodArray[i].name).classList.add("hidden");
+                //remove this food from the array
+                foodArray.splice(i, 1);
+                //break out of for loop, we found our food
+                break;
+            }
+        }
+    }
 }
 
 /*
@@ -79,11 +131,11 @@ var gamepiece = document.getElementById("gamepiece");
 
 
 gamepiece.classList.add("changeGreen");
-setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeGreen", ""); }, 3000);
+setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeGreen", ""); }, 1000);
 
 
 gamepiece.classList.add("changeRed");
-setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeRed", ""); }, 3000);
+setTimeout(function(){ gamepiece.className = gamepiece.className.replace("changeRed", ""); }, 1000);
 */
 
 
