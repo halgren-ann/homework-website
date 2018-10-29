795a67103048b5e480022644ccd9ced59e3a1478
Ann Halgren <annabellelarsen@gmail.com>
2018-10-29 12:28:39 -0700

    Working on making the gamepiece move with arrow keys

--- web/CIT261/FoodMaze/script.js
+++ web/CIT261/FoodMaze/script.js
@@ -34,8 +34,8 @@ function checkKey(e) {
 
     if (e.keyCode == '38') {
         // up arrow
-        if (document.getElementById("gamepiece").style.top > 50) {
-            document.getElementById("gamepiece").style.top -= 100;
+        if (document.getElementById("gamepiece").offsetTop > 50) {
+            document.getElementById("gamepiece").offsetTop -= 100; ;
         }
     }
     else if (e.keyCode == '40') {
