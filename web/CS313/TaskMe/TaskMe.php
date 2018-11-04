5307fb2ea9ad20f3a05f2cc1fdc58635ea001972
Ann Halgren <annabellelarsen@gmail.com>
2018-11-03 20:58:31 -0700

    Finalizing project

--- web/CS313/TaskMe/TaskMe.php
+++ web/CS313/TaskMe/TaskMe.php
@@ -118,32 +118,13 @@
         if (ev.target.tagName === 'LI') {
             ev.target.classList.toggle('checked');
             //change the database item is_complete with this information
-            if (ev.target.classList.contains('checked')) {
-                isChecked(ev.target.childNodes[0].textContent);
-            }
-            else {
-                isNotChecked(ev.target.childNodes[0].textContent);
-            }
+            isChecked(ev.target.childNodes[0].textContent);
         }
     }, false);
 
     function isChecked(task_text) {
         var httpc = new XMLHttpRequest(); // simplified for clarity
         var url = "isChecked.php";
-        httpc.open("POST", url, true); // sending as POST
-
-        httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        httpc.setRequestHeader("Content-Length", task_text.length); // POST request MUST have a Content-Length header (as per HTTP/1.1)
-
-        httpc.onreadystatechange = function() { //Call a function when the state changes.
-            if(httpc.readyState == 4 && httpc.status == 200) { }
-        }
-        httpc.send(task_text);
-    }
-
-    function isNotChecked(task_text) {
-        var httpc = new XMLHttpRequest(); // simplified for clarity
-        var url = "isNotChecked.php";
         httpc.open("POST", url, true); // sending as POST
 
         httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
