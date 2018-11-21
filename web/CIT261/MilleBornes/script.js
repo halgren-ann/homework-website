61dab3673117dcb6cc6076a69d1f7058d608a5c3
Ann Halgren <annabellelarsen@gmail.com>
2018-11-21 12:15:54 -0800

    Putting the whole card array on the board...

--- web/CIT261/MilleBornes/script.js
+++ web/CIT261/MilleBornes/script.js
@@ -1,5 +1,13 @@
 function newGame() {
-
+    //generate the random card stack
+    var cardArray = new Array();
+    cardArray = makeArray();
+    cardArray = shuffleArray(cardArray);
+    //populate the draw pile
+    for (var i=0; i<cardArray.length; i++) {
+        document.getElementById(cardArray[i].id).style = "z-index:" + i;
+    }
+    
 }
 
 function getInstructions() {
@@ -12,9 +20,107 @@ function getInstructions() {
 //type: attack, remedy, mile
 //location_class: relevant css classes (eg. UserCard1)
 //availableToDraw: true, false
-function card(name, type, location_class, availableToDraw) {
+function card(idNum, name, type, location_class, availableToDraw) {
+    this.id = "_" + idNum;
     this.name = name;
     this.type = type;
     this.location_class = location_class;
     this.availableToDraw = availableToDraw;
+}
+
+function makeArray() {
+    var cardArray = new Array();
+    var j=1;
+    for (var i=0; i<10; i++) {
+        newCard = new card(j, "25", "mile", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<10; i++) {
+        newCard = new card(j, "50", "mile", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<10; i++) {
+        newCard = new card(j, "75", "mile", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<12; i++) {
+        newCard = new card(j, "100", "mile", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<4; i++) {
+        newCard = new card(j, "200", "mile", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<14; i++) {
+        newCard = new card(j, "Drive", "remedy", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<5; i++) {
+        newCard = new card(j, "Stop", "attack", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "Gas", "remedy", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<3; i++) {
+        newCard = new card(j, "OutOfFuel", "attack", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "SpareTire", "remedy", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<3; i++) {
+        newCard = new card(j, "FlatTire", "attack", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "EndSpeedLimit", "remedy", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<4; i++) {
+        newCard = new card(j, "SpeedLimit", "attack", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "Repairs", "remedy", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<3; i++) {
+        newCard = new card(j, "Accident", "attack", "drawPile", true);
+        cardArray.push(newCard);
+        j++;
+    }
+
+    return cardArray;
+}
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
 }
