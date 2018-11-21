a0e74ea77d05ec71ffb2d300c146641afb9efa3a
Ann Halgren <annabellelarsen@gmail.com>
2018-11-20 21:09:20 -0800

    starting to add effects

--- web/CIT261/MilleBornes/script.js
+++ web/CIT261/MilleBornes/script.js
@@ -5,3 +5,16 @@ function newGame() {
 function getInstructions() {
 
 }
+
+//Make the card object
+//Valid values are as follows:
+//name: 25, 50, 75, 100, 200, Drive, Stop, Gas, OutOfFuel, SpareTire, FlatTire, EndSpeedLimit, SpeedLimit, Repairs, Accident
+//type: attack, remedy, mile
+//location_class: relevant css classes (eg. UserCard1)
+//availableToDraw: true, false
+function card(name, type, location_class, availableToDraw) {
+    this.name = name;
+    this.type = type;
+    this.location_class = location_class;
+    this.availableToDraw = availableToDraw;
+}
