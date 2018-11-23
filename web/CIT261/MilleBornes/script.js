7f661fa7dab5836d9d6301f12d752987f2c5cf3b
Ann Halgren <annabellelarsen@gmail.com>
2018-11-22 19:26:55 -0800

    Populating the PC's hand

--- web/CIT261/MilleBornes/script.js
+++ web/CIT261/MilleBornes/script.js
@@ -10,6 +10,7 @@ function newGame() {
     //populate the PC's hand
     for (var i=1; i<=6; i++) {
         document.getElementById(cardArray[cardArray.length-1].id).classList.remove("drawPile");
+        document.getElementById(cardArray[cardArray.length-1].id).style.zIndex = 0;
         document.getElementById(cardArray[cardArray.length-1].id).classList.add("PCCard" + i);
         cardArray[cardArray.length-1].availableToDraw = false; //do I need this?
         cardArray[cardArray.length-1].location_class = "PCCard" + i;
