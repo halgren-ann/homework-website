8b493cee635e3d71e8d0d263cb32621b738629fa
Ann Halgren <annabellelarsen@gmail.com>
2018-12-03 09:24:15 -0800

    trying out valid items for user card selection

--- web/CIT261/MilleBornes/script.js
+++ web/CIT261/MilleBornes/script.js
@@ -55,8 +55,11 @@ function getInstructions() {
 }
 
 //SECTION FOR USER PLAY LOGIC
+//test. Are there elements with the proper class names
+var list = document.getElementsByClassName("UserCard1");
+console.log("Number of elements with class UserCard1: "+list.length);
 //set an onclick listener for each user card
-(document.getElementsByClassName("UserCard1")[0]).addEventListener('click', function() {
+document.getElementsByClassName("UserCard1")[0].addEventListener('click', function() {
     selectCard(1);
 });
 document.getElementsByClassName("UserCard2")[0].addEventListener('click', function() {
