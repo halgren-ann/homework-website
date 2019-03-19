bb60150de322cb481529d431fbc6a5831ea5750d
Ann Halgren <annabellelarsen@gmail.com>
2019-03-19 00:45:31 -0700

    added cell padding

--- web/CIT261/ShoppingCart/index.php
+++ web/CIT261/ShoppingCart/index.php
@@ -15,7 +15,8 @@
     <h1 class="centered">Welcome to the sewing shop :)</h1>
     <p class="centered">Take a look around. We hope you find something you like!</p>
     <br/><hr>
-    <table class="nothing">
+
+    <table>
         <tr>
             <td class="col1">
             <div>
