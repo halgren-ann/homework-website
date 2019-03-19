1f32f55dc654642c51d6683285a5d1691011968f
Ann Halgren <annabellelarsen@gmail.com>
2019-03-18 23:42:46 -0700

    added cell padding

--- web/CIT261/ShoppingCart/index.php
+++ web/CIT261/ShoppingCart/index.php
@@ -18,24 +18,28 @@
     <table>
         <tr>
             <td class="col1">
+                <h2>Fabric</h2>
                 <img src="fabric.jpg"/>
                 <button onclick="addToCart('fabric')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('fabric')">Item Details</button>
             </td>
             <td class="col2">
+                <h2>Cutting Mat</h2>
                 <img src="mat.jpg"/>
                 <button onclick="addToCart('mat')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('mat')">Item Details</button>
             </td>
             <td class="col3">
+                <h2>Rotary Cutter</h2>
                 <img src="cutter.png"/>
                 <button onclick="addToCart('rotary')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('rotary')">Item Details</button>
             </td>
             <td class="col4">
+                <h2>Ruler</h2>
                 <img src="ruler.jpg"/>
                 <button onclick="addToCart('ruler')">Add to Cart</button>
                 <br/>
@@ -44,24 +48,28 @@
         </tr>
         <tr>
             <td class="col1">
+                <h2>Batting</h2>
                 <img src="batting.png"/>
                 <button onclick="addToCart('batting')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('batting')">Item Details</button>
             </td>
             <td class="col2">
+                <h2>Needles</h2>
                 <img src="needles.jpg"/>
                 <button onclick="addToCart('needles')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('needles')">Item Details</button>
             </td>
             <td class="col3">
+                <h2>Pins</h2>
                 <img src="pins.jpg"/>
                 <button onclick="addToCart('pins')">Add to Cart</button>
                 <br/>
                 <button onclick="getDetails('pins')">Item Details</button>
             </td>
             <td class="col4">
+                <h2>Sewing Machine</h2>
                 <img src="machine.jpg"/>
                 <button onclick="addToCart('machine')">Add to Cart</button>
                 <br/>
