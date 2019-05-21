b2ca5daba6926852c28d666834770325334df456
Ann Halgren <annabellelarsen@gmail.com>
2019-05-21 13:20:35 -0700

    Initial files. Sidebar, trying iframe

---
+++ web/SeniorProject/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Mille Bornes - Group Play</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+<div id="sidebar">
+    <iframe src="https://appr.tc/r/test_1" name="iframe_vid_1"></iframe>
+    <iframe src="https://appr.tc/r/test_1" name="iframe_vid_2"></iframe>
+    <iframe src="https://appr.tc/r/test_2" name="iframe_vid_3"></iframe>
+    <iframe src="https://appr.tc/r/test_2" name="iframe_vid_4"></iframe>
+</div>
+</body>
+<footer>
+</footer>
+</html>
