af1f34ed013ce995bcc81298c5ad361540f0d195
Ann Halgren <annabellelarsen@gmail.com>
2019-05-30 17:39:18 -0700

    setting out play area sections in css

--- web/SeniorProject/index.php
+++ web/SeniorProject/index.php
@@ -5,7 +5,7 @@
     <title>Mille Bornes - Group Play</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
-<body>
+<body onload="makeGame()" onclick="handleClick(event)">
 
 <div id="nonSidebar"></div>
 
@@ -25,16 +25,235 @@
 <div class="bottomRightPlayerSpeed cardOutline"></div>
 <div class="bottomRightPlayerMiles cardOutline"></div>
 
-<div class="playerHand1 cardOutline"></div>
-<div class="playerHand2 cardOutline"></div>
-<div class="playerHand3 cardOutline"></div>
-<div class="playerHand4 cardOutline"></div>
-<div class="playerHand5 cardOutline"></div>
-<div class="playerHand6 cardOutline"></div>
-<div class="playerHand7 cardOutline"></div>
-
 <div class="drawPile cardOutline"></div>
 <div class="discardPile cardOutline"></div>
+
+<?php
+        for ($i=1; $i <= 10; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="25.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=11; $i <= 20; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card  card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="50.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=21; $i <= 30; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="75.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=31; $i <= 42; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="100.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=43; $i <= 46; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="200.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=47; $i <= 60; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="drive.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=61; $i <= 65; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="stop.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=66; $i <= 71; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="gas.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=72; $i <= 74; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="outOfGas.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=75; $i <= 80; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="spareTire.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=81; $i <= 83; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="flatTire.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=84; $i <= 89; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="endSpeedLimit.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=90; $i <= 93; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="speedLimit.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=94; $i <= 99; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="repairs.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+
+        for ($i=100; $i <= 102; $i++) {
+            echo '
+            <div id="_' . $i . '" class="flip-card card drawPile">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="accident.png">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="cardBack.png">
+                    </div>
+                </div>
+            </div>
+            ';
+        }
+    ?>
 
 <div id="sidebar">
     <iframe src="https://appr.tc/r/wow_1" allow-popups allow="microphone; camera" name="iframe_vid_1"></iframe>
---
+++ web/SeniorProject/script.js
@@ -0,0 +1,124 @@
+//cardArray is the draw deck
+var cardArray = new Array(); 
+/*var PCHandArray = new Array();
+var UserHandArray = new Array();
+var PCDriveArray = new Array();
+var UserDriveArray = new Array();
+var PCSpeedArray = new Array();
+var UserSpeedArray = new Array();
+var PCMilesArray = new Array();
+var UserMilesArray = new Array();*/
+var discardPileArray = new Array();
+//items relevant to game play logic
+var isUserTurn = false;
+var haveDrawn = false;
+var selectedCard = null;
+var afterGame = false;
+var validArray = new Array();
+
+function makeGame() {
+    //generate the random card stack
+    cardArray = makeArray();
+    cardArray = shuffleArray(cardArray);
+    //populate the draw pile
+    for (var i=0; i<cardArray.length; i++) {
+        document.getElementById(cardArray[i].id).style = "z-index:" + (i+1);
+    }
+}
+
+function makeArray() {
+    var cardArray = new Array();
+    var j=1;
+    for (var i=0; i<10; i++) {
+        newCard = new card(j, "25", "mile");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<10; i++) {
+        newCard = new card(j, "50", "mile");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<10; i++) {
+        newCard = new card(j, "75", "mile");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<12; i++) {
+        newCard = new card(j, "100", "mile");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<4; i++) {
+        newCard = new card(j, "200", "mile");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<14; i++) {
+        newCard = new card(j, "Drive", "remedy");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<5; i++) {
+        newCard = new card(j, "Stop", "attack");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "Gas", "remedy");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<3; i++) {
+        newCard = new card(j, "OutOfFuel", "attack");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "SpareTire", "remedy");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<3; i++) {
+        newCard = new card(j, "FlatTire", "attack");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "EndSpeedLimit", "remedy");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<4; i++) {
+        newCard = new card(j, "SpeedLimit", "attack");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<6; i++) {
+        newCard = new card(j, "Repairs", "remedy");
+        cardArray.push(newCard);
+        j++;
+    }
+    for (var i=0; i<3; i++) {
+        newCard = new card(j, "Accident", "attack");
+        cardArray.push(newCard);
+        j++;
+    }
+
+    return cardArray;
+}
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+}
