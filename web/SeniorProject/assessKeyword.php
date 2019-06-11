c127313155bcbdeb8825f0d7a6ad063d22d79f46
Ann Halgren <annabellelarsen@gmail.com>
2019-06-10 22:53:24 -0700

    Finished the case where the keyword triggers the person joining an existing game

--- web/SeniorProject/assessKeyword.php
+++ web/SeniorProject/assessKeyword.php
@@ -32,8 +32,14 @@ if ($rows[0]) {
             VALUES (:game_id, :player_number, :display_name, :is_turn, :score);');
         $stmt->execute(array(':game_id' => $rows[0]["game_id"], ':player_number' => $rows[0]["num_players"], ':display_name' => $display_name, ':is_turn' => 'false', ':score' => '0'));
         $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        //Grab the player_id
+        $stmt = $db->prepare('SELECT * FROM public.player WHERE game_id = :game_id AND player_number = :player_number;');
+        $stmt->execute(array(':game_id' => $rows[0]["game_id"], ':player_number' => $rows[0]["num_players"]));
+        $newRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         //Return the information in JSON format
-        echo '{"player_id":' . '"TODO, get the correct player_id here"' . ', "player_number":' . $num_players . '}';
+        echo '{"player_id":' . $newRows[0]["player_id"] . ', "player_number":' . $num_players . '}';
     }
     else {
         //There are already 4 players, return "error"
