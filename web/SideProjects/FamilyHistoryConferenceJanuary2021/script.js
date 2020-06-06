7853c4a6f8b8d7ab48507e7352ed4d09b474adf0
Ann Halgren <annabellelarsen@gmail.com>
2020-06-06 05:09:28 -0700

    working on registration black box popup and checkbox visibility

--- web/SideProjects/FamilyHistoryConferenceJanuary2021/script.js
+++ web/SideProjects/FamilyHistoryConferenceJanuary2021/script.js
@@ -1,6 +1,6 @@
 function registerButtonClicked() {
     document.getElementById("registerPopupSection").classList.toggle("hidden");
     for(var i=0; i<document.getElementsByClassName("registrationCol").length; i++) {
-        document.getElementsByClassName("registrationCol")[i].visibility = "visible";
+        document.getElementsByClassName("registrationCol")[i].style.visibility = "visible";
     }
 }
