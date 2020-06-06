0aa7811165d91d25f99b9bbbad38ef3d8dffd50a
Ann Halgren <annabellelarsen@gmail.com>
2020-06-06 11:50:01 -0700

    Working on submitting registration data through an html form to a php page

--- web/SideProjects/FamilyHistoryConferenceJanuary2021/index.php
+++ web/SideProjects/FamilyHistoryConferenceJanuary2021/index.php
@@ -1,9 +1,35 @@
 <!DOCTYPE html>
 <?php 
     include 'dbConnect.php';
+    //Get the class list from the database to display in the table
     $stmt = $db->prepare('SELECT * FROM public.class ORDER BY class_time ASC;');
     $stmt->execute();
+    $classList = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //WORK DONE WHEN FORM IS SUBMITTED FOR REGISTRATION
+    //Add new attendee
+    $stmt = $db->prepare('INSERT into public.attendee(full_name, email) 
+        VALUES (:full_name, :email);');
+    $stmt->execute(array(':full_name' => $_POST["full_name"], ':email' => $_POST["email"]));
+    $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //Grab the attendee's id
+    $stmt = $db->prepare('SELECT * FROM public.attendee WHERE email =:email;');
+    $stmt->execute(array(':email' => $_POST["email"]));
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $attendee_id = $rows[0]["id"];
+
+    //Couple the attendee with the classes they registered for
+    foreach($_POST as $key => $value)
+    {
+        if (is_numeric($key))
+        {
+            $stmt = $db->prepare('INSERT into public.registered(attendee_id, class_id) 
+                VALUES (:attendee_id, :class_id);');
+            $stmt->execute(array(':attendee_id' => $attendee_id, ':class_id' => $key));
+            $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
 ?>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
@@ -13,7 +39,7 @@
     <link rel="stylesheet" type="text/css" href="styles.css">    
 </head>
 <body>
-    <form action="handleRegistration.php" method="post">
+    <form action="<?php=$_SERVER['PHP_SELF'];?>" method="post">
     <div id="opaqueContentBox" class="centered"></div>
     <h1 class="centered" id="title">Family History Conference</h1>
     <br/><br/><br/>
@@ -50,12 +76,12 @@
                 for ($i=0; $i < sizeof($rows); $i++) {
                     echo '
                         <tr>
-                        <td class=registrationCol style="padding: 2px"><input type="checkbox" name="' . $rows[$i]["id"] . '"></td>
-                        <td>' . date("h:i a", strtotime($rows[$i]["class_time"])) . '</td>
-                        <td>' . $rows[$i]["topic"] . '</td>
-                        <td>' . $rows[$i]["teacher"] . '</td>
-                        <td><a href="' . $rows[$i]["link"] . '">' . $rows[$i]["link"] . '</a></td>
-                        <td>' . $rows[$i]["additional_materials"] . '</td>
+                        <td class=registrationCol style="padding: 2px"><input type="checkbox" name="' . $classList[$i]["id"] . '"></td>
+                        <td>' . date("h:i a", strtotime($classList[$i]["class_time"])) . '</td>
+                        <td>' . $classList[$i]["topic"] . '</td>
+                        <td>' . $classList[$i]["teacher"] . '</td>
+                        <td><a href="' . $classList[$i]["link"] . '">' . $classList[$i]["link"] . '</a></td>
+                        <td>' . $classList[$i]["additional_materials"] . '</td>
                         </tr>
                     ';
                 }
