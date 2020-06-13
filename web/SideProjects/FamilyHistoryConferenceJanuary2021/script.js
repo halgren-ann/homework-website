765371db2503996f2de4d8071e5a9afe7f7cbcd6
Ann Halgren <annabellelarsen@gmail.com>
2020-06-13 15:52:33 -0700

    Working on email specifics

--- web/SideProjects/FamilyHistoryConferenceJanuary2021/script.js
+++ web/SideProjects/FamilyHistoryConferenceJanuary2021/script.js
@@ -1,15 +1,15 @@
 function openRegisterPopupSection() {
     document.getElementById("registerPopupSection").classList.toggle("hidden");
     for(var i=0; i<document.getElementsByClassName("registrationCol").length; i++) {
-        //document.getElementsByClassName("registrationCol")[i].style.visibility = "visible";
-        document.getElementsByClassName("registrationCol")[i].style.display = "block";
+        document.getElementsByClassName("registrationCol")[i].style.visibility = "visible";
+        //document.getElementsByClassName("registrationCol")[i].style.display = "block";
     }
 }
 
 function closeRegisterPopupSection() {
     document.getElementById("registerPopupSection").classList.toggle("hidden");
     for(var i=0; i<document.getElementsByClassName("registrationCol").length; i++) {
-        //document.getElementsByClassName("registrationCol")[i].style.visibility = "collapse";
-        document.getElementsByClassName("registrationCol")[i].style.display = "hidden";
+        document.getElementsByClassName("registrationCol")[i].style.visibility = "collapse";
+        //document.getElementsByClassName("registrationCol")[i].style.display = "hidden";
     }
 }
