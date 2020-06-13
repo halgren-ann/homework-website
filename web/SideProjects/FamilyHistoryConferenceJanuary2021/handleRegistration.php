e925d293a38673d981437d48e82a0c089f21c365
Ann Halgren <annabellelarsen@gmail.com>
2020-06-13 15:15:43 -0700

    Working on email specifics

--- web/SideProjects/FamilyHistoryConferenceJanuary2021/handleRegistration.php
+++ web/SideProjects/FamilyHistoryConferenceJanuary2021/handleRegistration.php
@@ -19,7 +19,8 @@
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $attendee_id = $rows[0]["id"];
 
-    //Couple the attendee with the classes they registered for
+    //Couple the attendee with the classes they registered for, and save the info for the email
+    $emailClassContent = null;
     foreach($_POST as $key => $value)
     {
         if (is_numeric($key))
@@ -28,28 +29,40 @@
                 VALUES (:attendee_id, :class_id);');
             $stmt->execute(array(':attendee_id' => $attendee_id, ':class_id' => $key));
             $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            //For email purposes
+            $stmt = $db->prepare('SELECT * FROM public.class(class_time, topic, teacher, link, additional_materials) 
+                WHERE id=:id;');
+            $stmt->execute(array(':id' => $key));
+            $classContent = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $emailClassContent += "Class time: " . $classContent[0]["class_time"] . "\n";
+            $emailClassContent += "Topic: " . $classContent[0]["topic"] . "\n";
+            $emailClassContent += "Teacher: " . $classContent[0]["teacher"] . "\n";
+            $emailClassContent += "Link: " . $classContent[0]["link"] . "\n";
+            $emailClassContent += "Any Additional Materials: " . $classContent[0]["additional_materials"] . "\n\n";
         }
     }
 
     /////////////////////////////////////////////////////////////////////
     //Send an email with the info about the classes they signed up for
-    /*// the message
-    $msg = "Hi Developer!";
-
-    // use wordwrap() if lines are longer than 70 characters
-    $msg = wordwrap($msg,70);
-
-    // send email
-    if(mail("annabellelarsen@gmail.com","Test",$msg)) {
-        //Redirect back to the main page   
-        header("Location:index.php");
-    }*/
-
     
     $from = new SendGrid\Email(null, "annabellelarsen@gmail.com");
-    $subject = "Hello World from the SendGrid PHP Library!";
-    $to = new SendGrid\Email(null, "annabellelarsen@gmail.com");
-    $content = new SendGrid\Content("text/plain", "Hello, Email!");
+    $subject = "Family History Conference Registration";
+    $to = new SendGrid\Email(null, $_POST["email"]);
+
+    //Create the content of the email
+    $contentStr = "Hi " . $_POST["full_name"] . ",\n\nThank you for registering for Family History Conference.";
+    $contentStr += "Below are the classes you are registered for that will take place on Saturday January 2021.";
+    $contentStr += "If you change your mind and would like to sign up for a different set of classes, ";
+    $contentStr += "just return to (link website here) and register again. Your class select will be reset with ";
+    $contentStr += "the new information.\n\n";
+    $contentStr += $emailClassContent;
+    $contentStr += "If you have any questions, please contact (contact info here). We look forward to seeing you!";
+    
+    $contentStr = wordwrap($contentStr,70);
+
+    $content = new SendGrid\Content("text/plain", $contentStr);
     $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
     $apiKey = getenv('SENDGRID_API_KEY');
@@ -59,41 +72,10 @@
     echo $response->statusCode();
     echo $response->headers();
     echo $response->body();
-    
-
-    /*$request_body = json_decode('{
-        "personalizations": [
-          {
-            "to": [
-              {
-                "email": "test@example.com"
-              }
-            ],
-            "subject": "Hello World from the SendGrid PHP Library!"
-          }
-        ],
-        "from": {
-          "email": "test@example.com"
-        },
-        "content": [
-          {
-            "type": "text/plain",
-            "value": "Hello, Email!"
-          }
-        ]
-      }');
-      
-      $apiKey = getenv('SENDGRID_API_KEY');
-      $sg = new \SendGrid($apiKey);
-      
-      $response = $sg->client->mail()->send()->post($request_body);
-      echo $response->statusCode();
-      echo $response->body();
-      echo $response->headers();*/
     ///////////////////////////////////////////////////////////////////
 
     //Redirect back to the main page   
-    //header("Location:index.php");
+    header("Location:index.php");
 ?>
 
 </body>
