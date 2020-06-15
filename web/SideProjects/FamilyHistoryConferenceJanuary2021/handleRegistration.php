62310c82a542fc5f9ea2f8f7341e9d5344ffcd1d
Ann Halgren <annabellelarsen@gmail.com>
2020-06-14 18:21:44 -0700

    Working on email specifics

--- web/SideProjects/FamilyHistoryConferenceJanuary2021/handleRegistration.php
+++ web/SideProjects/FamilyHistoryConferenceJanuary2021/handleRegistration.php
@@ -10,12 +10,12 @@
     //Add new attendee
     $stmt = $db->prepare('INSERT into public.attendee(full_name, email) 
         VALUES (:full_name, :email);');
-    $stmt->execute(array(':full_name' => $_POST["full_name"], ':email' => $_POST["email"]));
+    $stmt->execute(array(':full_name' => test_input($_POST["full_name"]), ':email' => test_input($_POST["email"])));
     $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     //Grab the attendee's id
     $stmt = $db->prepare('SELECT * FROM public.attendee WHERE email =:email;');
-    $stmt->execute(array(':email' => $_POST["email"]));
+    $stmt->execute(array(':email' => test_input($_POST["email"])));
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $attendee_id = $rows[0]["id"];
 
@@ -27,12 +27,12 @@
         {
             $stmt = $db->prepare('INSERT into public.registered(attendee_id, class_id) 
                 VALUES (:attendee_id, :class_id);');
-            $stmt->execute(array(':attendee_id' => $attendee_id, ':class_id' => $key));
+            $stmt->execute(array(':attendee_id' => $attendee_id, ':class_id' => test_input($key)));
             $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             //For email purposes
             $stmt = $db->prepare('SELECT * FROM public.class WHERE id=:id;');
-            $stmt->execute(array(':id' => $key));
+            $stmt->execute(array(':id' => test_input($key)));
             $classContent = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             $emailClassContent = $emailClassContent . 'Class time: ' . $classContent[0]["class_time"] . "\n";
@@ -48,11 +48,11 @@
     
     $from = new SendGrid\Email(null, "annabellelarsen@gmail.com");
     $subject = "Family History Conference Registration";
-    $to = new SendGrid\Email(null, $_POST["email"]);
+    $to = new SendGrid\Email(null, test_input($_POST["email"]));
 
     //Create the content of the email
     $contentStr = "";
-    $contentStr = $contentStr . "Hi " . $_POST["full_name"] . ",\n\nThank you for registering for Family History Conference.";
+    $contentStr = $contentStr . "Hi " . test_input($_POST["full_name"]) . ",\n\nThank you for registering for Family History Conference.";
     $contentStr = $contentStr . " Below are the classes you are registered for that will take place on Saturday January 2021.";
     $contentStr = $contentStr . " If you change your mind and would like to sign up for a different set of classes, ";
     $contentStr = $contentStr . "just return to (link website here) and register again. Your class selection will be reset with ";
@@ -76,6 +76,15 @@
 
     //Redirect back to the main page   
     header("Location:index.php");
+
+
+    //Security checks
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 ?>
 
 </body>
